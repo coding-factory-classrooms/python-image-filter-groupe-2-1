344fbf22aece0bfb846f72a22975db09c4500874
Léna <lenapancher@gmail.com>
2020-11-24 15:52:21 +0100

    story 2 filtre blur

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ cv2.imshow('Original image', image)
 black_white = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Gray image', black_white)
 
-status_black_white = cv2.imwrite('output/meme.png', black_white)
-print("Image written to file-system : ", status_black_white)
+blur = cv2.GaussianBlur(image, (15, 15), 0)
+
+#status_black_white = cv2.imwrite('output/meme.png', black_white)
+status_blur = cv2.imwrite('output/meme.png', blur)
+#print("Image written to file-system black and white : ", status_black_white)
+print("Image written to file-system blur : ", status_blur)
+
 
