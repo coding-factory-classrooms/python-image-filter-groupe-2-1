5d26c84e91cd7b448d377974990b4934038cd2dc
Léna <lenapancher@gmail.com>
2020-11-26 10:25:59 +0100

    story 9 fichier de configuration

--- cli.py
+++ cli.py
@@ -1,28 +1,29 @@
-import os
 import sys
 import main
+import configparser
 
 args = sys.argv
 print(f"args={args}")
 
-first_arg = args[1]
+if args[1] == "-h":
+    print("usage: imagefilter \n -h,--help \n -i,--img-dir <directory> \n -o,--output-dir <directory> \n -f,--filters")
+elif args[1] == "-i" and args[3] == "-o" and args[5] == "-f":
+    input_arg = args[2]
+    output_arg = args[4]
+    filter_arg = args[6]
+    main.filter(input_arg, output_arg, filter_arg)
+elif args[1] == "--config-file" and args[2] == "image.ini":
+    config = configparser.ConfigParser()
+    config.read("image.ini")
+    with open('image.ini', 'w') as configfile:
+        config.write(configfile)
+        content = config['filters']['content']
+        input = config['general']["input_dir"]
+        output = config['general']["output_dir"]
+        main.filter(input, output, content)
 
-if first_arg == "-h":
-    print("usage: imagefilter \n -h,----help \n -i,--img-dir <directory> \n -o,--output-dir <directory> \n -f, --filters")
-elif first_arg == "-i":
-    second_arg = args[2]
-    with os.scandir(second_arg) as entries:
-        if os.path.exists(second_arg):
-            for entry in entries:
-                print(entry.name)
-elif first_arg == "-o":
-    second_arg = args[2]
-    with os.scandir(second_arg) as entries:
-        if os.path.exists(second_arg):
-            for entry in entries:
-                print(entry.name)
-elif first_arg == "-f":
-    second_arg = args[2]
-    main.filter(second_arg)
+
+
+
 
 
--- filtres/dilate.py
+++ filtres/dilate.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def dilatation(image, iterations):
+def dilate(image, iterations):
     kernel = np.ones((5, 5), np.uint8)
     dilatation = cv2.dilate(image, kernel, iterations=iterations)
     return dilatation
